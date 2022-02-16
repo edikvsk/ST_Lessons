5537233ba8c8b9d7f1216988a5370de5105403ab
Eduard Vasilkovskii <e.vasilkovskii@systtech.ru>
2022-02-16 15:23:37 +0200

    Fix_remarks

--- main.py
+++ main.py
@@ -10,25 +10,25 @@ class GuessTheNumber:
     def get_random_num(self):
         return random.randint(self.LOWER_NUM, self.HIGHER_NUM)
 
-    # метод начала игры
+    # метод запускающий игру
     def play(self):
         random_num = self.get_random_num()
 
-        print(f"Угадай число в диапазоне от {self.LOWER_NUM} до {self.HIGHER_NUM}")
+        print(f"Угадай целое число в диапазоне от {self.LOWER_NUM} до {self.HIGHER_NUM}")
 
         # игровая механика
-        attempts = 0
+        attempts = 1
         while True:
             try:
                 input_num = int(input("Введи число: "))
             except ValueError:
-                print('--> Неверное значение, попробуй снова.')
-                guessTheNumberGame.play()
+                print('--> Неверное значение, счет попыток обнулен.')
+                self.play()
                 break
 
             if input_num == random_num:
                 print(
-                    f"Верное число {random_num}. Угадано с {attempts + 1}-ого раза")
+                    f"Верное число {random_num}. Угадано с {attempts}-ого раза")
                 break
 
             elif input_num < random_num:
