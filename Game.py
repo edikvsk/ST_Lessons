7b870bad7f486a878e1df67e7464408e5b68c39b
Eduard Vasilkovskii <e.vasilkovskii@systtech.ru>
2022-03-11 08:57:24 +0200

    Добавлены аннотации

--- Game.py
+++ Game.py
@@ -3,9 +3,9 @@ from random import randint
 
 class Game:
     def __init__(self):
-        self.LOWER_NUM = 1
-        self.HIGHER_NUM = 10
-        self.get_random_num = randint(self.LOWER_NUM, self.HIGHER_NUM)
+        self.LOWER_NUM: int = int(1)
+        self.HIGHER_NUM: int = int(10)
+        self.get_random_num: int = int(randint(self.LOWER_NUM, self.HIGHER_NUM))
         self.start_game()
 
 
